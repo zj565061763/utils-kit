f3cba0e84d6decc0585a54b55ab03dd9960a46aa
zj565061763 <565061763@qq.com>
2022-12-09 15:17:15 +0800

    FPackageUtils

--- lib/src/main/java/com/sd/lib/utilskit/FPackageUtils.kt
+++ lib/src/main/java/com/sd/lib/utilskit/FPackageUtils.kt
@@ -1,14 +1,30 @@
 package com.sd.lib.utilskit
 
+import android.content.pm.PackageInfo
+import android.content.pm.PackageManager
+import android.content.pm.PackageManager.PackageInfoFlags
+import android.os.Build
 import com.sd.lib.ctx.fContext
+
+private val packageManager = fContext.packageManager
+
+/**
+ * 当前App的包信息
+ */
+fun fMyPackageInfo(flags: Int = PackageManager.GET_CONFIGURATIONS): PackageInfo {
+    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+        packageManager.getPackageInfo(fContext.packageName, PackageInfoFlags.of(flags.toLong()))
+    } else {
+        packageManager.getPackageInfo(fContext.packageName, flags)
+    }
+}
 
 /**
  * 启动某个App，默认启动自身
  */
 fun fStartApp(packageName: String = fContext.packageName) {
     try {
-        val manager = fContext.packageManager
-        val intent = manager.getLaunchIntentForPackage(packageName)
+        val intent = packageManager.getLaunchIntentForPackage(packageName)
         fContext.startActivity(intent)
     } catch (e: Exception) {
         e.printStackTrace()
