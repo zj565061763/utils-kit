b07daa665d2ace7c1ce35c14ca6f66e47b53e926
zj565061763 <565061763@qq.com>
2022-12-15 14:15:00 +0800

    fResIdOfDrawable

--- lib/src/main/java/com/sd/lib/utilskit/FResUtils.kt
+++ lib/src/main/java/com/sd/lib/utilskit/FResUtils.kt
@@ -6,11 +6,12 @@ import com.sd.lib.ctx.fContext
  * 根据文件名获取图片资源id
  */
 fun fResIdOfDrawable(name: String): Int? {
-    return try {
+    val resId = try {
         val resources = fContext.resources
         resources.getIdentifier(name, "drawable", fContext.packageName)
     } catch (e: Exception) {
         e.printStackTrace()
-        null
+        0
     }
+    return if (resId == 0) null else resId
 }
